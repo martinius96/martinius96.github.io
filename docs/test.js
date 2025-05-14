71ae26251a8d78e88bbee0752b0e0d7b226f243a
Martin Chlebovec <martinius96@gmail.com>
2025-05-14 23:27:02 +0200

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -1,8 +1,15 @@
-<script type='text/javascript'>
-  window.smartlook||(function(d) {
-    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-    })(document);
-    smartlook('init', 'eafab5dd1bf0a73ad262bbb9b04a02eab3f3c15a', { region: 'eu' });
-</script>
+(function(d) {
+  var o = window.smartlook = function() {
+    o.api.push(arguments);
+  };
+  var h = d.getElementsByTagName('head')[0];
+  var c = d.createElement('script');
+  o.api = [];
+  c.async = true;
+  c.type = 'text/javascript';
+  c.charset = 'utf-8';
+  c.src = 'https://web-sdk.smartlook.com/recorder.js';
+  h.appendChild(c);
+})(document);
+
+smartlook('init', 'eafab5dd1bf0a73ad262bbb9b04a02eab3f3c15a', { region: 'eu' });
